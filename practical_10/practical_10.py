e81c05bc286465d1b58c2f731dea06ae34ab40b8
TECHTANIC <64263079+techtanic@users.noreply.github.com>
2020-11-22 17:27:02 +0300

    .

--- practical_10/practical_10.py
+++ practical_10/practical_10.py
@@ -11,72 +11,98 @@ e)	Delete a record
 
 f)	Exit'''
 
-
 import pickle
 
-def insert():
-    f = open("bin.dat","ab")
-    rollno = int(input("Roll no: "))
+try:
+    with open("bin.dat", "rb") as f:
+        student_record = pickle.load(f)
+except FileNotFoundError:
+    student_record = {}
+
+
+def save():
+    with open("bin.dat", "wb") as f:
+        pickle.dump(student_record, f)
+
+
+def insert_record():
+    roll_no = int(input("Roll Number: "))
     name = input("Name: ")
-    marks = int(input("marks: "))
-    l = [rollno, name, marks]
-    pickle.dump(l, f)
-    f.close()
-    
-def display():
-    f = open("bin.dat","rb")
-    try:
-        while True:
-            l = pickle.load(f)
-            print(l)
-    except EOFError:
-        f.close()
-    
-def search():
-    f = open("bin.dat","rb")
-    rollno = int(input("Roll no: "))
-    try:
-        while True:
-            l = pickle.load(f)
-            if l[0] == rollno:
-                print(l)
-                break
-    except EOFError:
-        print("Rollno not found")
-        f.close()
+    marks = input("Marks: ")
+    print()
 
-def update():
-    f = open("bin.dat","rb+")
-    rollno = int(input("Roll no: "))def search():
-    f = open("bin.dat","rb")
-    rollno = int(input("Roll no: "))
-    try:
-        while True:
-            l = pickle.load(f)
-            if l[0] == rollno:
-                print(l)
-                break
-    except EOFError:
-        print("Rollno not found")
-        f.close()
-    try:
-        while True:
-            l = pickle.load(f)
-            if l[0] == rollno:
-                marks = 
-                break
-    except EOFError:
-        print("Rollno not found")
-        f.close()
+    student_record[roll_no] = [name, marks]
+    print("Record added.")
 
-#menu
-print("1. Insert record\n2. Diplay record\n3. Search rollno\n4. Update marks\n5. Delete record\n6. Exit")
+
+def display_records():
+    if len(student_record) == 0:
+        print("No students found.")
+    else:
+        print("Roll No | Name | Marks\n")
+        for student in student_record:
+            print(student, *student_record[student], sep=" | ")
+
+
+def search_for_record():
+    roll_no = int(input("Roll Number to search for: "))
+    print()
+
+    if roll_no in student_record:
+        print(roll_no, *student_record[roll_no], sep=" | ")
+    else:
+        print("Student not found.")
+
+
+def update_marks():
+    roll_no = int(input("Roll Number: "))
+    print()
+
+    if roll_no in student_record:
+        print(roll_no, *student_record[roll_no], sep=" | ")
+        marks = int(input("New Marks: "))
+        student_record[roll_no][1] = marks
+    else:
+        print("Student not found.")
+
+
+def delete_record():
+    roll_no = int(input("Roll Number: "))
+    print()
+
+    if roll_no in student_record:
+        print(roll_no, *student_record[roll_no], sep=" | ")
+        confirm = input("Confirm delete? (y/n): ")
+        if confirm.lower() == "y":
+            del student_record[roll_no]
+    else:
+        print("Student not found.")
+
+
+
+print("1: Insert Record")
+print("2: Display Records")
+print("3: Search based on Roll Number")
+print("4: Update Marks")
+print("5: Delete Record")
+print("6: Exit\n")
+
 while True:
-    ch = int(input("Choice: "))
-    if ch == 1:
-        insert()
-    if ch == 2:
-        display()
-    if ch == 3:
-        search()
-        
+    choice = int(input("Choice: "))
+    print()
+
+    if choice == 1:
+        insert_record()
+    if choice == 2:
+        display_records()
+    if choice == 3:
+        search_for_record()
+    if choice == 4:
+        update_marks()
+    if choice == 5:
+        delete_record()
+    if choice == 6:
+        break
+
+    save()
+    print()
